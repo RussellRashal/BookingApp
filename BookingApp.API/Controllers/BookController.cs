3d27f722cfb3e78cefca857c3600dd69844ef610
Rashal <rashal1@hotmail.com>
2020-04-04 21:09:06 +0100

    NewBookVersion2

--- BookingApp.API/Controllers/BookController.cs
+++ BookingApp.API/Controllers/BookController.cs
@@ -1,11 +1,16 @@
-
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using BookingApp.API.Data;
+using System.Threading.Tasks;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using BookingApp.API.Data;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+
+using BookingApp.API.Models;
+
+
 using Microsoft.EntityFrameworkCore;
 
 
@@ -26,36 +31,167 @@ namespace BookingApp.API.Controllers
         [HttpGet]
         public async Task<IActionResult> GetBooking()
         {
-            var Booking = await _context.bookings.ToListAsync();
+            var Booking = await _context.bookings.Include(c => c.Customer).ToListAsync();
 
             return Ok(Booking);
         }
+
+
+
 
         [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetBooking(int id)
         {
             var Booking = await _context.bookings.FirstOrDefaultAsync(x => x.Id == id);
-            
+
             return Ok(Booking);
         }
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
+    //  POST api/values
+       
+          // if (bookings.Time && Table == exists)
+             // return BadRequest
+        //return Ok(bookings)
+
+            // if(bookings has already been booked)
+            //     return BadRequest("booking already booked");
+            // return Ok(bookings);
+            // using select queries WHere date = 1.00 and table = 1
+            // return true         Task<IActionResult>
+
+
+        // [AllowAnonymous]
+        // [HttpPost]
+        // public async Task<IList<Booking>> Checked(string Request, Booking bookings)
+        // {
+        //     var SearchData = await _context.bookings.
+        //     Where(x => x.Title == Request).ToListAsync();
+
+
+        //     return Ok(Booking);
+
+
+
+
+        // }
+
+    [AllowAnonymous]
+    [HttpPost]
+    public async Task<IActionResult> PostBook(Booking bookings)
+    {
+        // var titleExists = 
+        //     await _context.bookings.AnyAsync(b => b.Title == bookings.Title);
+
+        // if (titleExists) 
+        //     return BadRequest($"Title '{bookings.Title}' already exists");
+
+         var TimeExists = 
+            await _context.bookings.AnyAsync(b => b.NoPeople  == bookings.NoPeople);
+        
+        var titleExists = 
+            await _context.bookings.AnyAsync(b => b.Title == bookings.Title); 
+            
+         
+         if (TimeExists  && titleExists) 
+            return BadRequest(" already exists");
+        
+                
+        
+        
+            _context.bookings.Add(bookings);
+            await _context.SaveChangesAsync();
+
+             return Ok();
+    
+    
+    }  
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    [AllowAnonymous]
+    [HttpPut("{Id}")]
+    public Booking PutBooking(int Id, [FromBody] Booking bookings )
+    {
+        var bookingsInDb = _context.bookings.SingleOrDefault(b => b.Id == Id);
+
+
+        bookingsInDb.Title = bookings.Title;
+        bookingsInDb.Date = bookings.Date;
+        bookingsInDb.Request = bookings.Request;
+
+
+
+        _context.SaveChanges();
+
+        return bookingsInDb;
+
+    }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       
+
+
+
+
+
+
+
+
+
+
 
         // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+        // [HttpPut("{id}")]
+
+        
+        //
+
+        //
+
+
+        // }
+
+
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
         }
-    }
+}
+
+
 }
